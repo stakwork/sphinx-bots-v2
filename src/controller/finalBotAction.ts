36656b5f06d2fc0f5fc28980d2d1843e0d21758c
Oluwatobi Bamidele <teebams49@gmail.com>
2024-06-17 21:37:07 +0100

    send action to mixer

--- src/controller/finalBotAction.ts
+++ src/controller/finalBotAction.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { config } from "../config";
 import logger from "../logger";
 import { Action } from "../types";
 
@@ -5,4 +7,18 @@ export async function finalAction(a: Action): Promise<void> {
   logger.info(`Send message to mixer: ${JSON.stringify(a)}`);
 
   //Send via API
+  if (config.mixer_url) {
+    try {
+      await axios.post(
+        config.mixer_url,
+        { ...a },
+        { headers: { "x-secret": config.mixer_secret_key } }
+      );
+      logger.info("Action sent to Mixer successfully");
+    } catch (error) {
+      logger.error(`Error posting Action to Mixer: ${JSON.stringify(error)}`);
+    }
+  } else {
+    logger.error("MIXER_URL is not set in environment variable");
+  }
 }
