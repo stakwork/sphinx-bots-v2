f74128889dfdc668909dc4d58efb0d144e7a0c3f
Oluwatobi Bamidele <teebams49@gmail.com>
2024-06-18 23:02:21 +0100

    refactor: made little inversion change for sending message to mixer

--- src/controller/finalBotAction.ts
+++ src/controller/finalBotAction.ts
@@ -7,18 +7,19 @@ export async function finalAction(a: Action): Promise<void> {
   logger.info(`Send message to mixer: ${JSON.stringify(a)}`);
 
   //Send via API
-  if (config.mixer_url) {
-    try {
-      await axios.post(
-        config.mixer_url,
-        { ...a },
-        { headers: { "x-secret": config.mixer_secret_key } }
-      );
-      logger.info("Action sent to Mixer successfully");
-    } catch (error) {
-      logger.error(`Error posting Action to Mixer: ${JSON.stringify(error)}`);
-    }
-  } else {
+  if (!config.mixer_url) {
     logger.error("MIXER_URL is not set in environment variable");
+    return;
+  }
+
+  try {
+    await axios.post(
+      config.mixer_url,
+      { ...a },
+      { headers: { "x-secret": config.mixer_secret_key } }
+    );
+    logger.info("Action sent to Mixer successfully");
+  } catch (error) {
+    logger.error(`Error posting Action to Mixer: ${JSON.stringify(error)}`);
   }
 }
