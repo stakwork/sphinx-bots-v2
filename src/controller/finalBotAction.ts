469404d4fd60fc3d89b1a1480bc4ce71c8e5505e
Evan Feenstra <evanfeenstra@gmail.com>
2024-08-08 09:16:18 -0700

    fix merge

--- src/controller/finalBotAction.ts
+++ src/controller/finalBotAction.ts
@@ -6,19 +6,18 @@ import { Action } from "../types";
 export async function finalAction(a: Action): Promise<void> {
   logger.info(`Send message to mixer: ${JSON.stringify(a)}`);
 
-  //Send via API
-  if (config.mixer_url) {
-    try {
-      await axios.post(
-        config.mixer_url,
-        { ...a },
-        { headers: { "x-admin-token": config.mixer_secret_key } }
-      );
-      logger.info("Action sent to Mixer successfully");
-    } catch (error) {
-      logger.error(`Error posting Action to Mixer: ${JSON.stringify(error)}`);
-    }
-  } else {
+  if (!config.mixer_url) {
     logger.error("MIXER_URL is not set in environment variable");
+    return;
+  }
+  try {
+    await axios.post(
+      config.mixer_url,
+      { ...a },
+      { headers: { "x-admin-token": config.mixer_secret_key } }
+    );
+    logger.info("Action sent to Mixer successfully");
+  } catch (error) {
+    logger.error(`Error posting Action to Mixer: ${JSON.stringify(error)}`);
   }
 }
